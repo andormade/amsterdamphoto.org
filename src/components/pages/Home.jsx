d2b9ada81c9fc92d74d68c36efc40c1dc3b727a2
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-04-24 22:44:28 +0200

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,79 +11,16 @@ export default () => (
 		image="home/1.jpg"
 		permalink="/"
 	>
-			<PrettyLink
-				to="/blog/2017/09/12/photo-walk-amsterdam-haarlem"
-				title="Photo walks"
-				color="black"
-				background="white"
-				image="home/1.jpg"
-			>
-				<Photo file="home/1.jpg" />
-			</PrettyLink>
+		
+		<Photo file="home/1.jpg" />
+		<Photo file="home/2.jpg" />
+		<Photo file="home/3.jpg" />
+		<Photo file="home/4.jpg" />
+		<Photo file="home/5.jpg" />
+		<Photo file="home/6.jpg" />
+		<Photo file="home/7.jpg" />
 
-
-		<PrettyLink
-			to="/couple_shoots"
-			title="Romantic couple sessions"
-			color="white"
-			background="black"
-			image="home/2.jpg"
-		>
-			<Photo file="home/2.jpg" />
-		</PrettyLink>
-
-
-		<PrettyLink
-			to="/couple_shoots"
-			title="Engagement & pre-wedding sessions"
-			color="white"
-			background="black"
-			image="home/3.jpg"
-		>
-			<Photo file="home/3.jpg" />
-		</PrettyLink>
-
-			<PrettyLink
-				to="/portraits"
-				title="Beautiful magazine-like portraits"
-				color="black"
-				background="white"
-				image="home/4.jpg"
-			>
-				<Photo file="home/4.jpg" />
-			</PrettyLink>
-
-		<PrettyLink
-			to="/couple_shoots"
-			title="Engagement & pre-wedding sessions"
-			color="white"
-			background="black"
-			image="home/5.jpg"
-		>
-			<Photo file="home/5.jpg" />
-		</PrettyLink>
-
-
-		<PrettyLink
-			to="/couple_shoots"
-			title="Romantic flower field couple session"
-			color="white"
-			background="black"
-			image="home/6.jpg"
-		>
-			<Photo file="home/6.jpg" />
-		</PrettyLink>
-
-			<PrettyLink
-			to="/bridal_beauty"
-			title="Beautiful styled bridal portraits"
-			color="white"
-			background="black"
-			image="home/7.jpg"
-		>
-			<Photo file="home/7.jpg" />
-		</PrettyLink>
-
+	
 
 		<CallToActionButton to="/contact" text="Book now" />
 	</AlbumPage>
