c35d499052e365c7dffc375fbc3b1d6cd13b9e7b
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-05-27 16:06:25 +0200

    Update AboutMe.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -10,13 +10,13 @@ export default () => (
 			alt="Annie Kostolany Photographer"
 		/>
 
-		<p>Hey there!</p>
+		<p>Hi!</p>
 
 		<p>
 			My name is Annie, I am Hungarian couple & lifestyle photographer
 			living in Amsterdam. 
 			<br /> My inspiration comes from my travels and all the amazing people I have met during my journey. 
-			I'm specialized in portrait photography.
+			I specialize in portrait photography.
 			<br />My style is clean, airy and bright with a romantic touch.
 			If this is what you're looking for, I'm happy to send you my package & information guide. 
 			
