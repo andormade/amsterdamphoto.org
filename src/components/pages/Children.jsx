bc53a8c41b1ebbc83640382c6f694e495d5924d4
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-06-18 23:44:43 +0200

    Update Children.jsx

--- src/components/pages/Children.jsx
+++ src/components/pages/Children.jsx
@@ -6,19 +6,14 @@ import CallToActionButton from '../CallToActionButton';
 export default () => (
 	<TextPage
 		title="Children photography"
-		description="Capturing the best moments of a kid's life in definitely not an easy task. It requires some patience and understanding..."
+		description="Photographing children is definitely not an easy task. It requires some patience and understanding..."
 		image="children/kids1.jpg"
 	>
 		<h2> Children photography </h2>
 		<p>
-			Capturing the best moments of a kid's life in definitely not an easy
-			task. It requires some patience and understanding. I believe that no
-			matter how much energy a child has, it is always possible to achieve
-			good results with them. I'll happily photograph birthdays, cake
-			smashes, themed mini sessions (Christmas, costumes) and events. I
-			have my own home-studio in Haarlem but I am flexible on the
-			location. Contact me for more information on prices and
-			availability.
+			Photographing children is definitely not an easy task. Sometimes it requires a lot of patience and understanding. 
+			I photograph birthdays, cake smashes, themed mini sessions (Christmas, costumes) and events. I work confidently in my home studio in Haarlem just as much as on-location. 
+			Contact me for more information on prices and availability.
 		</p>
 		<Photo file="children/kids1.jpg" />
 		<Photo file="children/kids2.jpg" />
