f90f577303aea9f565dd7209c1f5f31d3e055b35
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-06-24 18:20:31 +0200

    Update Couples.jsx

--- src/components/posts/Couples.jsx
+++ src/components/posts/Couples.jsx
@@ -9,25 +9,32 @@ export default () => (
 		description="Romantic couple portrait sessions to celebrate love & happiness"
 		date="2018-03-22"
 		permalink="couple-sessions"
-		image="couple_shoots/2b.jpg"
+		image="couple_shoots/daria.jpg"
 	>
-
+	
 		
-		<Photo file="couple_shoots/1.jpg" />
+		<p>
+     I simply LOVE to photograph couples. I always seek for their most perfect, authentic moments to capture their love. 
+		</p>
+		
+		
+		<Photo file="couple_shoots/daria.jpg" />
+		<Photo file="couple_shoots/0.jpg" />
+		<Photo file="couple_shoots/00.jpg" />
+		<Photo file="couple_shoots/000.jpg" />
+		<Photo file="couple_shoots/daria2.jpg" />
 		<Photo file="couple_shoots/2.jpg" />
-		<Photo file="couple_shoots/2b.jpg" />
 		<Photo file="couple_shoots/3.jpg" />
+		 <p>
+      In-home sessions are a great way to showcase your personality. Your apartment, choice of color and design add a lot to the photos. 
+    </p>
 		<Photo file="couple_shoots/4.jpg" />
 		<Photo file="couple_shoots/5.jpg" />
 		<Photo file="couple_shoots/6.jpg" />
-		<Photo file="couple_shoots/7.jpg" />
+    	<Photo file="couple_shoots/7.jpg" />
 		<Photo file="couple_shoots/8.jpg" />
-		<Photo file="couple_shoots/8b.jpg" />
 		<Photo file="couple_shoots/9.jpg" />
-		<Photo file="couple_shoots/10.jpg" />
-		<Photo file="couple_shoots/10b.jpg" />
-    	<Photo file="couple_shoots/11.jpg" />
-		<Photo file="couple_shoots/12.jpg" />
+    	<Photo file="couple_shoots/10.jpg" />
     
     <p>Message me to book your own photo session.</p>
 	</BlogPost>
