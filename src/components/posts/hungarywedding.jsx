2f822cbe7449190b650ab31b9aa372edf1d0b9b9
3n1k0 <32710850+3n1k0@users.noreply.github.com>
2018-08-28 18:55:14 +0200

    Create hungarywedding.jsx

---
+++ src/components/posts/hungarywedding.jsx
@@ -0,0 +1,60 @@
+
+import BlogPost from '../BlogPost';
+import Photo from '../Photo';
+import React from 'react';
+
+export default () => (
+	<BlogPost
+		title="Dreamy Summer Wedding in Hungary"
+		description=" "
+		date="2018-08-28"
+		permalink="summer-wedding-hungary"
+		image="blog/wedding1/38.jpg"
+	>
+		<p>Eszter and Isti are a beautiful couple from Hungary. Although I am based in Amsterdam and I do most of my work here, I am always happy to travel and challange myself in a new environment. 
+    Their wedding was just like a fairytale, and I was really lucky to be there with them on this wonderful day. I still remember my own wedding, which was three years ago and I know exactly how difficult it is to organize one. 
+    However, these memories are everlasting, we carry them not only on the pictures but in our hearts as well. 
+		</p
+		
+    <Photo file="blog/wedding1/1.jpg" />
+    <Photo file="blog/wedding1/2.jpg" />
+    <Photo file="blog/wedding1/3.jpg" />
+    <Photo file="blog/wedding1/4.jpg" />
+    <Photo file="blog/wedding1/5.jpg" />
+    <Photo file="blog/wedding1/6.jpg" />
+    <Photo file="blog/wedding1/7.jpg" />
+    <Photo file="blog/wedding1/8.jpg" />
+    <Photo file="blog/wedding1/9.jpg" />
+    <Photo file="blog/wedding1/10.jpg" />
+    <Photo file="blog/wedding1/11.jpg" />
+    <Photo file="blog/wedding1/12.jpg" />
+    <Photo file="blog/wedding1/13.jpg" />
+    <Photo file="blog/wedding1/14.jpg" />
+    <Photo file="blog/wedding1/15.jpg" />
+    <Photo file="blog/wedding1/16.jpg" />
+    <Photo file="blog/wedding1/17.jpg" />
+    <Photo file="blog/wedding1/18.jpg" />
+    <Photo file="blog/wedding1/19.jpg" />
+    <Photo file="blog/wedding1/20.jpg" />
+    <Photo file="blog/wedding1/21.jpg" />
+    <Photo file="blog/wedding1/22.jpg" />
+    <Photo file="blog/wedding1/23.jpg" />
+    <Photo file="blog/wedding1/24.jpg" />
+    <Photo file="blog/wedding1/25.jpg" />
+    <Photo file="blog/wedding1/26.jpg" />
+    <Photo file="blog/wedding1/28.jpg" />
+    <Photo file="blog/wedding1/29.jpg" />
+    <Photo file="blog/wedding1/30.jpg" />
+    <Photo file="blog/wedding1/31.jpg" />
+    <Photo file="blog/wedding1/32.jpg" />
+    <Photo file="blog/wedding1/33.jpg" />
+    <Photo file="blog/wedding1/34.jpg" />
+    <Photo file="blog/wedding1/35.jpg" />
+    <Photo file="blog/wedding1/36.jpg" />
+    <Photo file="blog/wedding1/37.jpg" />
+    <Photo file="blog/wedding1/38.jpg" />
+    
+   
+    
+	</BlogPost>
+);
